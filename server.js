3d3f1640a113e57f2de2fb4a3bb93c8062dc62e4
Johannes Jambor <jambor@billiger-mietwagen.de>
2018-01-26 10:13:58 +0100

    add wireframe

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const SocketServer = require("ws").Server;
+const path = require("path");
+
+const PORT = process.env.port || 3000;
+const INDEX = path.join(__dirname, "public/tv_index.html"); // determine by user-agent
+
+const server = express()
+    .use((request, response) => response.sendFile(INDEX))
+    .listen(PORT, () => console.log(`listening on port ${PORT}`));
+
+const webSocketServer = new SocketServer({server});
+
+webSocketServer.on("connection", webSocket => {
+    console.log("client connected");
+    webSocket.on("close", () => console.log("client disconnected"));
+});
+
+setInterval(() => {
+    webSocketServer.clients.forEach(client => {
+        client.send(new Date().toTimeString());
+    });
+});
