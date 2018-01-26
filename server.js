dedfa5d25e9db21e9ce705c35bd05f89891fa46d
Johannes Jambor <jambor@billiger-mietwagen.de>
2018-01-26 12:59:17 +0100

    configure static-files; add static-filenames for js/css

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const app = express();
-const expressWs = require("express-ws")(app);
+const expressWs = require("express-ws")(app, null, {
+    wsOptions: {
+        clientTracking: true
+    }
+});
 
 const path = require("path");
-
 const PORT = process.env.port || 5000;
+
+app.use(express.static("public"));
 
 app.get("/", (request, response) => {
     const isMobileDevice = request.headers["user-agent"].match(/Mobile/);
@@ -21,8 +26,26 @@ app.listen(PORT, () => {
     console.log(`listening on port ${PORT}`)
 });
 
+let members = [];
+let clientPool = [];
+
 app.ws("/", (ws, request) => {
+    ws.on("open", () => {
+        console.log("new connection");
+        clientPool.push(ws);
+    });
+
     ws.on("message", message => {
         console.log("received", message);
+
+        const messageObject = JSON.parse(message);
+
+        switch (messageObject.payload.action) {
+            case "addMember":
+                console.log("addMember", messageObject.payload.name);
+                members.push(messageObject.payload.name)
+                console.log("current members:", members);
+                break;
+        }
     });
 });
