af90ba3197dfeace2cc92aade19f4b5d41b60f56
Johannes Jambor <jambor@billiger-mietwagen.de>
2018-01-30 20:16:39 +0100

    use 2-space indentation

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const app = express();
 const expressWs = require("express-ws")(app, null, {
-    wsOptions: {
-        clientTracking: true
-    }
+  wsOptions: {
+    clientTracking: true
+  }
 });
 const _ = require("lodash");
 
@@ -13,18 +13,18 @@ const PORT = process.env.port || 5000;
 app.use(express.static("public"));
 
 app.get("/", (request, response) => {
-    const isMobileDevice = request.headers["user-agent"].match(/Mobile/);
-    const indexPath = isMobileDevice
-        ? "public/mobile_index.html"
-        : "public/tv_index.html";
+  const isMobileDevice = request.headers["user-agent"].match(/Mobile/);
+  const indexPath = isMobileDevice
+    ? "public/mobile_index.html"
+    : "public/tv_index.html";
 
-    const INDEX = path.join(__dirname, indexPath);
+  const INDEX = path.join(__dirname, indexPath);
 
-    response.sendFile(INDEX);
+  response.sendFile(INDEX);
 });
 
 app.listen(PORT, () => {
-    console.log(`listening on port ${PORT}`)
+  console.log(`listening on port ${PORT}`)
 });
 
 const webSocketServer = expressWs.getWss();
@@ -37,62 +37,63 @@ const webSocketServer = expressWs.getWss();
 let state = {};
 
 const broadcastState = () => {
-    // `{"origin": "web-socket-server", "action": "setNumbers", "payload": {"numbers": ${JSON.stringify(numbers)}}}`;
-    const broadcastPayload = {
-        origin: "web-socket-server",
-        action: "setState",
-        state
-    };
+  // `{"origin": "web-socket-server", "action": "setNumbers", "payload": {"numbers": ${JSON.stringify(numbers)}}}`;
+  const broadcastPayload = {
+    origin: "web-socket-server",
+    action: "setState",
+    state
+  };
 
-    const message = JSON.stringify(broadcastPayload);
+  const message = JSON.stringify(broadcastPayload);
 
-    console.log("--> broadcasting", message);
+  console.log("--> broadcasting", message);
 
-    webSocketServer.clients.forEach(client => {
-        client.send(message);
-    });
+  webSocketServer.clients.forEach(client => {
+    client.send(message);
+  });
 };
 
 app.ws("/", (ws, request) => {
-    ws.on("message", message => {
-        console.log("receiving", message);
+  ws.on("message", message => {
+    console.log("receiving", message);
 
-        const messageObject = JSON.parse(message);
+    const messageObject = JSON.parse(message);
 
-        switch (messageObject.payload.action) {
-            case "selectDeveloper":
-                console.log("--> selectDeveloper", messageObject.payload.name);
+    switch (messageObject.payload.action) {
+      case "selectDeveloper":
+        console.log("--> selectDeveloper", messageObject.payload.name);
 
-                if (_.has(state, messageObject.payload.name)) {
-                    break;
-                }
-
-                state[messageObject.payload.name] = null;
-
-                console.log("--> current state:", state);
-                broadcastState();
-                break;
-            case "resetDeveloperSelection":
-                console.log("--> resetDeveloperSelection", messageObject.payload.name);
-
-                delete state[messageObject.payload.name];
-
-                console.log("--> current state:", state);
-                broadcastState();
-                break;
-            case "selectEstimation":
-                console.log("--> selectEstimation", messageObject.payload.name, messageObject.payload.estimation);
-
-                state[messageObject.payload.name] = messageObject.payload.estimation;
-
-                console.log("--> current state:", state);
-                broadcastState();
-                break;
-            case "reset":
-                console.log("--> reset");
-                state = {};
-                broadcastState();
-                break;
+        if (_.has(state, messageObject.payload.name)) {
+          console.log(`--> ${messageObject.payload.name} already in state ...`);
+          break;
         }
-    });
+
+        state[messageObject.payload.name] = null;
+
+        console.log("--> current state:", state);
+        broadcastState();
+        break;
+      case "resetDeveloperSelection":
+        console.log("--> resetDeveloperSelection", messageObject.payload.name);
+
+        delete state[messageObject.payload.name];
+
+        console.log("--> current state:", state);
+        broadcastState();
+        break;
+      case "selectEstimation":
+        console.log("--> selectEstimation", messageObject.payload.name, messageObject.payload.estimation);
+
+        state[messageObject.payload.name] = messageObject.payload.estimation;
+
+        console.log("--> current state:", state);
+        broadcastState();
+        break;
+      case "reset":
+        console.log("--> reset");
+        state = {};
+        broadcastState();
+        break;
+    }
+  });
 });
